ff0bd5648a593a6b266a5cdaf5dd33b1db005e18
Mani vardhan <manivardhan12356@gmail.com>
2023-10-30 18:53:06 +0530

    added

--- backend/controllers/productsController.js
+++ backend/controllers/productsController.js
@@ -1,25 +1,40 @@
-import express from 'express';
+import express from 'express';  // Import the 'express' module
 import asyncHandler from 'express-async-handler';
-import { fileURLToPath } from 'url';
-import path from 'path';
-import fs from 'fs/promises';
+import  data from '../config/JSON/data.json' assert { type: 'json' };
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
-const dataPath = path.join(__dirname, '../config/JSON/data.json');
-
-const app = express();
+const app = express();  // Create an Express.js app
 
 const productsData = asyncHandler(async (req, res) => {
-   try {
-      const data = await fs.readFile(dataPath, 'utf-8');
-      const jsonData = JSON.parse(data);
-      return res.json({ data: jsonData });
-   } catch (error) {
-      console.error('Error reading or parsing JSON:', error);
-      return res.status(500).json({ error: 'Internal Server Error' });
-   }
+   return res.json(data);  // Send JSON data
 });
 
-export { productsData };
+
+export { productsData }
+
+
+// import express from 'express';
+// import asyncHandler from 'express-async-handler';
+// import { fileURLToPath } from 'url';
+// import path from 'path';
+// import fs from 'fs/promises';
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+// const dataPath = path.join(__dirname, '../config/JSON/data.json');
+
+// const app = express();
+
+// const productsData = asyncHandler(async (req, res) => {
+//    try {
+//       const data = await fs.readFile(dataPath, 'utf-8');
+//       const jsonData = JSON.parse(data);
+//       return res.json({ data: jsonData });
+//    } catch (error) {
+//       console.error('Error reading or parsing JSON:', error);
+//       return res.status(500).json({ error: 'Internal Server Error' });
+//    }
+// });
+
+// export { productsData };
