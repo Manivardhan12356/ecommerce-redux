0a354d04dabb74682b07fe8bc6a34eb2865c26cd
Mani vardhan <manivardhan12356@gmail.com>
2023-10-30 19:31:38 +0530

    made sme changes

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,16 @@
-import path from 'path'
-import express from "express";
-import dotenv from 'dotenv'
-import userRouter from './routes/userRouter.js'
-import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
-import connectDB from "./config/db.js";
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+import userRouter from './routes/userRouter.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import cookieParser from 'cookie-parser';
-import cors from 'cors'
+import cors from 'cors';
 
-dotenv.config()
-const port = process.env.PORT || 5000
-connectDB();
+dotenv.config();
+const port = process.env.PORT || 5000;
+
+connectDB(); // Connect to the MongoDB database
+
 const app = express();
 app.use(cors());
 
@@ -19,21 +20,13 @@ app.use(cookieParser());
 
 app.use('/api/users', userRouter);
 
-if (process.env.NODE_ENV !== 'production') {
-   const _dirname = path.resolve();
-   app.use(express.static(path.join(_dirname, 'frontend/dist')));
-   app.get('*',(req, res) =>res.sendFile(path.resolve(__dirname,"frontend",'dist','index.html')));
-} else {
-   app.get('/', (req, res) => {
-      res.send("iam runing")
-   })
-}
 
+   app.get('/', (req, res) => {
+      res.send('I am running');
+   });
 
 
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(port, () => console.log(`running on port ${port}`));
-
-
+app.listen(port, () => console.log(`Server is running on port ${port}`));
