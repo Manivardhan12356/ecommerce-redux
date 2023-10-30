f9abb3492cd4f079af67e9fbe7ee7b6e65aa718b
Mani vardhan <manivardhan12356@gmail.com>
2023-10-30 19:43:21 +0530

    added

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,12 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+   useNewUrlParser: true,
+   useCreateIndex: true,
+   useUnifiedTopology: true,
+   extended: true
+}));
 app.use(cookieParser());
 
 app.use('/api/users', userRouter);
