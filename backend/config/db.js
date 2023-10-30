eccd9df2e7855077b511539d5f20573faa289b50
Mani vardhan <manivardhan12356@gmail.com>
2023-10-30 22:01:34 +0530

    added

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,24 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const connectDB = async () => {
    try {
-      const con = await mongoose.connect(process.env.MONGO_URL, {
+      const url = process.env.MONGO_URL;
+      if (!url) {
+         throw new Error('MongoDB connection URL is not defined');
+      }
+
+      const options = {
          useNewUrlParser: true,
          useUnifiedTopology: true,
-      })
-      console.log(`mongo db connected ${con.connection.host}`)
+         // Additional mongoose options if required
+      };
+
+      const con = await mongoose.connect(url, options);
+      console.log(`MongoDB connected: ${con.connection.host}`);
    } catch (error) {
-      console.error(`Error: ${error.message}`)
+      console.error(`Error: ${error.message}`);
       process.exit(1);
    }
-}
-
+};
 
 export default connectDB;
